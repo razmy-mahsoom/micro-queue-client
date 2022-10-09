c688d1f992627ca61c1dca871a3dba658db7e4fe
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-09 13:55:14 +0530

    version changed to 1.0.0

--- src/main/java/org/microq/client/MicroQueueClientApplication.java
+++ src/main/java/org/microq/client/MicroQueueClientApplication.java
@@ -1,13 +1,18 @@
 package org.microq.client;
 
+import org.microq.client.config.ApplicationConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@PropertySource(value = "classpath:micro-q-client-property.yml",factory = ApplicationConfiguration.class)
 public class MicroQueueClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MicroQueueClientApplication.class, args);
-    }
 
 }
