727e6a232261de9fa1466895b5730d98e097dd61
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-09 13:44:51 +0530

    initial commit

---
+++ src/main/java/org/microq/client/MicroQueueClientApplication.java
@@ -0,0 +1,13 @@
+package org.microq.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroQueueClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroQueueClientApplication.class, args);
+    }
+
+}
