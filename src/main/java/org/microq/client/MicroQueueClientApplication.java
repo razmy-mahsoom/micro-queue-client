29bef41c9dc6bb46ebf92a0278014bbe562e1ad0
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-11 11:34:54 +0530

    default interchange and sequence responsibility is moved to server

--- src/main/java/org/microq/client/MicroQueueClientApplication.java
+++ src/main/java/org/microq/client/MicroQueueClientApplication.java
@@ -1,9 +1,7 @@
 package org.microq.client;
 
 import org.microq.client.config.ApplicationConfiguration;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
