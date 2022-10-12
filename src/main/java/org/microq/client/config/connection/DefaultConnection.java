ae51268cfcd2c7f414fee55fd26a21e3f697f879
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-12 19:14:39 +0530

    default interchange and sequence responsibility is moved to server

---
+++ src/main/java/org/microq/client/config/connection/DefaultConnection.java
@@ -0,0 +1,38 @@
+package org.microq.client.config.connection;
+
+import org.microq.support.config.connection.MQConnection;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Qualifier("defaultConnection")
+public class DefaultConnection implements MQConnection {
+    @Value("${micro-q-config.connection.username}")
+    private String username;
+    @Value("${micro-q-config.connection.password}")
+    private String password;
+    @Value("${micro-q-config.connection.host}")
+    private String host;
+    @Value("${micro-q-config.connection.port}")
+    private Integer port;
+    @Override
+    public String username() {
+        return username;
+    }
+
+    @Override
+    public String password() {
+        return password;
+    }
+
+    @Override
+    public String host() {
+        return host;
+    }
+
+    @Override
+    public Integer port() {
+        return port;
+    }
+}
---
+++ src/main/java/org/microq/client/config/connection/MQConnectionProcessor.java
@@ -0,0 +1,34 @@
+package org.microq.client.config.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.microq.support.config.connection.MQConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.Socket;
+
+@Configuration
+@Slf4j
+public class MQConnectionProcessor {
+
+    @Autowired
+    @Qualifier("defaultConnection")
+    private MQConnection connection;
+
+    @PostConstruct
+    public void startConnection(){
+      log.info("Starting Connection to MQ on host: {}, port: {}",connection.host(),connection.port());
+        try {
+            Socket socket = new Socket(connection.host(),connection.port());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MQConnection getConnection(){
+        return connection;
+    }
+}
