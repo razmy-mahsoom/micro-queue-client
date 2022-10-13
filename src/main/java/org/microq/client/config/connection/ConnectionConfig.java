1c7d76c874c29b3d3aef361d47d261f19ff5fde2
razmy-mahsoom <razmy_mahsoom@hotmail.com>
2022-10-13 16:03:26 +0530

    default interchange and sequence responsibility is moved to server

--- src/main/java/org/microq/client/config/connection/ConnectionConfig.java
+++ src/main/java/org/microq/client/config/connection/ConnectionConfig.java
@@ -1,16 +1,34 @@
 package org.microq.client.config.connection;
 
+import lombok.extern.slf4j.Slf4j;
 import org.microq.support.config.connection.MQConnection;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.io.IOException;
+import java.net.Socket;
 
 @Configuration
+@Slf4j
 public class ConnectionConfig {
 
-    @Bean
-    @ConditionalOnMissingBean(value = MQConnection.class)
+    @Bean("default-mq-connection")
     public MQConnection defaultMQConnection(){
         return new DefaultConnection();
     }
+
+    @Bean
+    @DependsOn("default-mq-connection")
+    public Socket socket(){
+        log.info("Starting Connection to MQ on host: {}, port: {}",defaultMQConnection().host(),defaultMQConnection().port());
+        try {
+            Socket socket = new Socket(defaultMQConnection().host(), defaultMQConnection().port());
+            log.info("Connected to MQ on {}",socket.getLocalSocketAddress());
+            return socket;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
--- src/main/java/org/microq/client/config/connection/MQConnectionProcessor.java
+++ src/main/java/org/microq/client/config/connection/MQConnectionProcessor.java
@@ -4,29 +4,39 @@ import lombok.extern.slf4j.Slf4j;
 import org.microq.support.config.connection.MQConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.Socket;
 
-@Configuration
+//@Configuration
 @Slf4j
 public class MQConnectionProcessor {
 
     @Autowired
     private MQConnection connection;
 
-    @PostConstruct
-    public void startConnection(){
-      log.info("Starting Connection to MQ on host: {}, port: {}",connection.host(),connection.port());
+//    @PostConstruct
+//    public void startConnection(){
+//      log.info("Starting Connection to MQ on host: {}, port: {}",connection.host(),connection.port());
+//        try {
+//            Socket socket = new Socket(connection.host(),connection.port());
+//            log.info("Connected to MQ on {}",socket.getLocalSocketAddress());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+   // @Bean
+    public Socket socket(){
         try {
-            Socket socket = new Socket(connection.host(),connection.port());
+            return new Socket(connection.host(),connection.port());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
     public MQConnection getConnection(){
         return connection;
     }
